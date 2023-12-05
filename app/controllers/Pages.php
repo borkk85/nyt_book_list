f3d5973b3b67ab7fa73b1811c208a4382dc5bc08
Borko <bor.kos.mk@gmail.com>
2023-12-05 11:55:52 +0100

    add .env to .gitignore

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -15,18 +15,33 @@ class Pages extends Controller
     $this->bookModel = new Book();
   }
 
-  public function index() {
-    
-          $books = $this->bookModel->getAllBooks();
+  public function index($selected_cat = null)
+  {
+    $selected_cat = isset($_GET['category']) ? $_GET['category'] : null;
+    $books = $this->bookModel->getAllBooks();
 
+    $categories = array_unique(array_reduce($books, function ($call, $book) {
+      return array_merge($call, explode(', ', $book['genres']));
+    }, []));
 
-        $this->view('pages/index', $books);
+    if ($selected_cat) {
+      $books = array_filter($books, function ($book) use ($selected_cat) {
+        return strpos($book['genres'], $selected_cat) !== false;
+      });
+    }
 
-}
+    $data = [
+      'books' => $books,
+      'categories' => $categories,
+      'selected_cat' => $selected_cat
+    ];
 
-
-  public function about() {
-    echo 'This is about';
+    $this->view('pages/index', $data);
   }
 
+
+  public function about()
+  {
+    echo 'This is about';
+  }
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,23 +1,41 @@
 <?php require dirname(__DIR__) . '/inc/header.php'; ?>
 
-<h1>Books</h1>
+<main>
+    <div class="hero-section">
+        <div class="hero-overlay">
+            <h2></h2>
+            <p></p>
+        </div>
+    </div>
+    <div class="book-section">
+        <nav class="category-nav">
+            <ul>
+                <li><a href="/nyt_book_list/";>All Books</a></li>
+                <?php foreach ($data['categories'] as $category) : ?>
+                    <li><a href="/nyt_book_list/?category=<?php echo urlencode($category); ?>"><?php echo htmlspecialchars($category); ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
 
-<?php if (!empty($data)): ?>
-    <ul>
-        <?php foreach ($data as $book): ?>
-            <li>
-                <h2><?php echo htmlspecialchars($book['title']); ?></h2>
-                <p>Author: <?php echo htmlspecialchars($book['author']); ?></p>
-                <p>ISBN: <?php echo htmlspecialchars($book['isbn13']); ?></p>
-                <p>Genres: <?php echo htmlspecialchars($book['genres']); ?></p>
-                <!-- Add more fields as needed -->
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php else: ?>
-    <p>No books found.</p>
-<?php endif; ?>
+        <?php if (!empty($data)) : ?>
+            <div class="book-listings">
+                <?php foreach ($data['books'] as $book) : ?>
+                    <!-- <?php echo '<pre>', var_dump($book), '</pre>'; ?> -->
+                    <div class="book">
+                        <img src="<?php echo $book['book_image_url']; ?>" alt="Book Cover">
+                        <div class="book-details">
+                            <h3><?php echo htmlspecialchars($book['title']); ?></h3>
+                            <p><?php echo htmlspecialchars($book['author']); ?></p>
+                            <p>New York Times Bestseller</p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
 
-<?php require dirname(__DIR__) . '/inc/footer.php'; ?>
+        <?php else : ?>
+            <p>No books found.</p>
+        <?php endif; ?>
 
+    </div>
 
+</main>
